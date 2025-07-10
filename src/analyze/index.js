9ff82fd3822b4c7976175504d263957797742894
Sameen Karim <sameenkarim@gmail.com>
2025-07-10 09:19:17 -0400

    fix stuck issue when tsconfig not found

--- src/analyze/index.js
+++ src/analyze/index.js
@@ -8,32 +8,59 @@ const path = require('path');
 const { parseCustomFunctionSignature } = require('./utils/customFunctionParser');
 const { getAllFiles } = require('../utils/fileProcessor');
 const { analyzeJsFile } = require('./javascript');
-const { analyzeTsFile } = require('./typescript');
+const { analyzeTsFiles } = require('./typescript');
 const { analyzePythonFile } = require('./python');
 const { analyzeRubyFile } = require('./ruby');
 const { analyzeGoFile } = require('./go');
 
-async function analyzeDirectory(dirPath, customFunctions) {
-  const allEvents = {};
+/**
+ * Adds an event to the events collection, merging properties if event already exists
+ * @param {Object} allEvents - Collection of all events
+ * @param {Object} event - Event to add
+ * @param {string} baseDir - Base directory for relative path calculation
+ */
+function addEventToCollection(allEvents, event, baseDir) {
+  const relativeFilePath = path.relative(baseDir, event.filePath);
+  
+  const implementation = {
+    path: relativeFilePath,
+    line: event.line,
+    function: event.functionName,
+    destination: event.source
+  };
 
-  const customFunctionSignatures = (customFunctions && customFunctions?.length > 0) ? customFunctions.map(parseCustomFunctionSignature) : null;
+  if (!allEvents[event.eventName]) {
+    allEvents[event.eventName] = {
+      implementations: [implementation],
+      properties: event.properties,
+    };
+  } else {
+    allEvents[event.eventName].implementations.push(implementation);
+    allEvents[event.eventName].properties = {
+      ...allEvents[event.eventName].properties,
+      ...event.properties,
+    };
+  }
+}
 
-  const files = getAllFiles(dirPath);
-
+/**
+ * Processes non-TypeScript files
+ * @param {Array<string>} files - Array of file paths
+ * @param {Object} allEvents - Collection to add events to
+ * @param {string} baseDir - Base directory for relative paths
+ * @param {Array} customFunctionSignatures - Custom function signatures to detect
+ */
+async function processOtherFiles(files, allEvents, baseDir, customFunctionSignatures) {
   for (const file of files) {
     let events = [];
 
     const isJsFile = /\.(jsx?)$/.test(file);
-    const isTsFile = /\.(tsx?)$/.test(file);
     const isPythonFile = /\.(py)$/.test(file);
     const isRubyFile = /\.(rb)$/.test(file);
     const isGoFile = /\.(go)$/.test(file);
 
     if (isJsFile) {
       events = analyzeJsFile(file, customFunctionSignatures);
-    } else if (isTsFile) {
-      // Pass null program so analyzeTsFile will create a per-file program using the file's nearest tsconfig.json
-      events = analyzeTsFile(file, null, customFunctionSignatures);
     } else if (isPythonFile) {
       events = await analyzePythonFile(file, customFunctionSignatures);
     } else if (isRubyFile) {
@@ -41,37 +68,43 @@ async function analyzeDirectory(dirPath, customFunctions) {
     } else if (isGoFile) {
       events = await analyzeGoFile(file, customFunctionSignatures);
     } else {
-      continue;
+      continue; // Skip unsupported file types
     }
 
-    events.forEach((event) => {
-      const relativeFilePath = path.relative(dirPath, event.filePath);
-
-      if (!allEvents[event.eventName]) {
-        allEvents[event.eventName] = {
-          implementations: [{
-            path: relativeFilePath,
-            line: event.line,
-            function: event.functionName,
-            destination: event.source
-          }],
-          properties: event.properties,
-        };
-      } else {
-        allEvents[event.eventName].implementations.push({
-          path: relativeFilePath,
-          line: event.line,
-          function: event.functionName,
-          destination: event.source
-        });
-
-        allEvents[event.eventName].properties = {
-          ...allEvents[event.eventName].properties,
-          ...event.properties,
-        };
-      }
-    });
+    events.forEach(event => addEventToCollection(allEvents, event, baseDir));
   }
+}
+
+async function analyzeDirectory(dirPath, customFunctions) {
+  const allEvents = {};
+
+  const customFunctionSignatures = (customFunctions?.length > 0) 
+    ? customFunctions.map(parseCustomFunctionSignature) 
+    : null;
+
+  const files = getAllFiles(dirPath);
+  
+  // Separate TypeScript files from others for optimized processing
+  const tsFiles = [];
+  const otherFiles = [];
+  
+  for (const file of files) {
+    const isTsFile = /\.(tsx?)$/.test(file);
+    if (isTsFile) {
+      tsFiles.push(file);
+    } else {
+      otherFiles.push(file);
+    }
+  }
+
+  // Process TypeScript files with optimized batch processing
+  if (tsFiles.length > 0) {
+    const tsEvents = analyzeTsFiles(tsFiles, customFunctionSignatures);
+    tsEvents.forEach(event => addEventToCollection(allEvents, event, dirPath));
+  }
+
+  // Process remaining file types
+  await processOtherFiles(otherFiles, allEvents, dirPath, customFunctionSignatures);
 
   return allEvents;
 }
